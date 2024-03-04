f0e1f760783fe90b4bb8d1ba513736891f9bd695
Julien Delange <julien.delange@datadoghq.com>
2024-03-04 17:04:30 -0500

    add

---
+++ misc/release.py
@@ -0,0 +1,23 @@
+# list files in the rules directory
+
+import os
+import sys
+import yaml
+
+
+files = os.listdir('rules')
+rules = []
+for file in files:
+    with open('rules/' + file, 'r') as f:
+        # parse yaml file and get the content
+        content = yaml.safe_load(f)
+        rules.append(content)
+
+content = {
+    "rules": rules
+}
+
+# write file
+with open('release.yaml', 'w') as f:
+    yaml.dump(content, f)
+    print('release.yaml has been created')
